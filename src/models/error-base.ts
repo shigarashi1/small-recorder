36925bf0d341fe357689abeb8efd1874eb8c1d71
sigarashi <50ra4.dev.dev@gmail.com>
2019-07-10 00:21:18 +0900

    create error class

---
+++ src/models/error-base.ts
@@ -0,0 +1,30 @@
+import { IError } from '../types/error';
+
+type TError = string | IError | IError[];
+
+export class ErrorBase<T = TError> extends Error {
+  private _errors: IError[] = [];
+
+  constructor(error: T) {
+    super();
+    if (typeof error === 'string') {
+      this._errors.push({ code: 'E0000', error });
+    } else if (Array.isArray(error)) {
+      this._errors = [...error];
+    } else if (isIError(error)) {
+      this._errors.push({ ...error });
+    }
+  }
+
+  get errors() {
+    return this._errors;
+  }
+
+  get message() {
+    return '';
+  }
+}
+
+function isIError(value: any): value is IError {
+  return value.hasOwnProperty('code');
+}
--- src/types/error.ts
+++ src/types/error.ts
@@ -2,20 +2,3 @@ export interface IError {
   code: string;
   error: string;
 }
-
-export class ErrorClass extends Error {
-  private _errors: IError[] = [];
-
-  constructor(error: string | IError[]) {
-    super();
-    if (typeof error === 'string') {
-      this._errors.push({ code: 'E0000', error });
-    } else {
-      this._errors = [...error];
-    }
-  }
-
-  get errors() {
-    return this._errors;
-  }
-}
