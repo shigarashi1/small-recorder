16d8e1fb2c49fed391ace185cfca02aa076835c4
sigarashi <sigarashi@sigarashinoMacBook.local>
2019-10-02 22:34:35 +0900

    create observer store

---
+++ src/store-observable/index.ts
@@ -0,0 +1,47 @@
+import thunk from 'redux-thunk';
+import { actionCreatorFactory } from 'typescript-fsa';
+import { combineEpics, createEpicMiddleware } from 'redux-observable';
+import { combineReducers, compose, applyMiddleware, StoreCreator, createStore, AnyAction } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+
+//
+import { sampleReducers, sampleEpics } from './_sample';
+import history from '../helpers/history';
+
+// actions
+const ac = actionCreatorFactory('[---- root]');
+const actions = {
+  allClear: ac<void>('allClear'),
+};
+
+// reducer
+const reducers = combineReducers({
+  router: connectRouter(history),
+  sample: sampleReducers,
+});
+
+export type AppState = ReturnType<typeof reducers>;
+
+const rootReducer = (state: any, action: any) => {
+  if (action.type === actions.allClear.type) {
+    return undefined;
+  }
+  return reducers;
+};
+
+// epic
+const rootEpic = combineEpics(sampleEpics);
+const epicMiddleware = createEpicMiddleware<AnyAction, AnyAction, AppState>();
+
+// enhance
+const enhancers = compose(
+  applyMiddleware(thunk, epicMiddleware, routerMiddleware(history)),
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    : (f: StoreCreator) => f,
+);
+
+export const configureStore = (initialState: any) => {
+  epicMiddleware.run(rootEpic);
+  return createStore(rootReducer, initialState, enhancers);
+};
